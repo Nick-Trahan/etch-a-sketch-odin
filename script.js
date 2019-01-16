39a9ecc8d990ea80f7f759307da6d75454988bc2
Nick Trahan <43890781+Nick-Trahan@users.noreply.github.com>
2019-01-16 09:32:35 -0600

    Change #screen to #div-screen, add comments to script.js

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
 const divContainer = document.querySelector('#container');
-const divScreen = document.querySelector('#screen');
+const divScreen = document.querySelector('#div-screen');
 const btnReset = document.querySelector('#button-reset')
 
 createGrid(16);
 
+//Creates the correct amount of divs by first assigning the user-provided number of columns and rows to the parent div, then looping though that user-provided number times itself. Each loop creates a div, assigns a class and id, then appends the div to its parent element.
 function createGrid(number) {
+  divScreen.setAttribute('style', 'grid-template: repeat('+ number + ', 1fr)' + ' \/ ' + 'repeat(' + number + ', 1fr)');
+
   for(let i = 0; i < number * number; i++) {
     const divBox = document.createElement('div');
 
     divBox.setAttribute('id', 'div-box-' + (i + 1));
     divBox.setAttribute('class', 'box');
 
-    divScreen.setAttribute('style', 'grid-template: repeat('+ number + ', 1fr)' + ' \/ ' + 'repeat(' + number + ', 1fr)');
     divScreen.appendChild(divBox);
   }
 }
 
-divScreen.addEventListener('mouseover', (event) => {
+//helper function for random color generator
+function random(number) { 
+  return Math.floor(Math.random() * (number + 1));
+}
+
+//generates a random color, then applies that color to the currently chosen div.
+divScreen.addEventListener('mouseover', (event) => { 
+  const randomColor = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
   const currentBox = event.target;
 
-  currentBox.style.backgroundColor = 'black';
+  currentBox.style.backgroundColor = randomColor;
 });
 
+
 btnReset.addEventListener('click', (event) => {
-  event.stopPropagation();
+  event.stopPropagation(); //stops this event from applying to parent element
   resetBoard();
 });
 
