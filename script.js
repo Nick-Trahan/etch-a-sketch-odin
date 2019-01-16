5455b0482a26fb5236f9f511f1110af1bfd8eaff
Nick-Trahan <nicktrahan@gmail.com>
2019-01-15 20:11:28 -0600

    Create mouseover effect

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const divContainer = document.querySelector('#container');
 const divScreen = document.querySelector('#screen');
 
+createGrid(16);
+
 function createGrid(number) {
   for(let i = 0; i < number * number; i++) {
     const divBox = document.createElement('div');
 
     divBox.setAttribute('id', 'divBox-' + (i + 1));
     divBox.setAttribute('class', 'box');
-    
+
     divScreen.setAttribute('style', 'grid-template: repeat('+ number + ', ' + '1fr' + ')' + ' \/ ' + 'repeat(' + number + ', ' + '1fr' + ')');
     divScreen.appendChild(divBox);
   }
 }
+
+divScreen.addEventListener('mouseover', (event) => {
+  const currentBox = event.target;
+  currentBox.style.backgroundColor = 'black';
+});
